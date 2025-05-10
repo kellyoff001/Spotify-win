d525b219f9f517d2b11113e47f835d7e5122a111
kellyoff001 <chriskelly8565@gmail.com>
2025-05-10 22:16:58 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 
 %SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "&{[Net.ServicePointManager]::SecurityProtocol = 3072}; """"& { $(Invoke-WebRequest -UseBasicParsing 'https://spotx-official.github.io/run.ps1')} -new_theme """" | Invoke-Expression"
 
